99941cebbda78265dd4f5cfaf24b15abece29a54
VomV <vivek.vkr.rawat@gmail.com>
2023-02-25 22:56:03 +0000

    add pos embed

--- Transformers/myGPT.py
+++ Transformers/myGPT.py
@@ -121,7 +121,8 @@ class BigramLanguageModel(nn.Module):
         #idx and targets are both (Batch ,Time) tensor of integers
         token_emb = self.token_embedding_table(idx) # (Batch=batch_size, Time=block_size, Channel=embed_size)
         pos_emb = self.position_embedding_table(torch.arange(T)) #(T,C)
-        logits = self.lm_head(token_emb) #(Batch=batch_size, Time=block_size, Channel=vocab_size)
+        x = token_emb + pos_emb
+        logits = self.lm_head(x) #(Batch=batch_size, Time=block_size, Channel=vocab_size)
 
         if targets is not None:
             B, T, C = logits.shape
