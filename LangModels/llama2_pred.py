5fcad0ad21eb44f123d7f11dcd5278d7c3356bb5
VomV <vivek.vkr.rawat@gmail.com>
2023-07-20 16:54:11 +0100

    comment

--- LangModels/llama2_pred.py
+++ LangModels/llama2_pred.py
@@ -2,6 +2,8 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 
+
+#Download v2 models
 model = "meta-llama/Llama-2-7b-chat-hf"
 
 tokenizer = AutoTokenizer.from_pretrained(model)
@@ -18,7 +20,6 @@ sequences = pipeline(
     top_k=10,
     num_return_sequences=1,
     eos_token_id=tokenizer.eos_token_id,
-    max_length=200,
-)
+    max_length=200,)
 for seq in sequences:
     print(f"Result: {seq['generated_text']}")
