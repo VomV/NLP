2580d37a6e78852b9f38770cb8c4b6b5cf6b1749
VomV <vivek.vkr.rawat@gmail.com>
2025-04-15 22:04:17 +0200

    Update doc_chat.py

--- RAG/DocChat/doc_chat.py
+++ RAG/DocChat/doc_chat.py
@@ -1,6 +1,7 @@
 """
 DocChat:::
 
+
 Conversational interface that integrates with LlamaIndex and OpenAI's GPT-3.5. 
 Users can ask questions about LlamaIndex Docs, and the application provides relevant answers. 
 ...
